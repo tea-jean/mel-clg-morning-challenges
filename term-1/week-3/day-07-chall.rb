92dabb33d7bc505a04d4d5dc2ef9eea0db14daaf
burntsugar <rrakky89@gmail.com>
2019-10-30 21:38:26 +1100

    Updated file

---
+++ term-1/week-3/day-07-chall.rb
@@ -0,0 +1,67 @@
+# Day 7 Morning Challenges
+############################
+
+# Challenge 1
+
+# Ask the user to enter a 24 hour based time as a float (so 5:38pm would be 17.38). If the user enters a number that is less than 12 print "Morning"; if the number entered is exactly 12, print "Midday"; if number is greater than 12 but less than or equal to 13 print "Lunch"; if the number is greater than 13, but less than 17, print "Afternoon"; if the number entered is less than or equal to 21 but more than or equal to 17 print "Evening"; if number is greater than 21 but less than or equal to 24 print "Time for bed". If they enter 0 or 24, print "Midnight". If the time isn't in the 24 hour range print "Not a valid 24 hour time".
+
+# You can add in more responses if you like.
+
+user_time = gets.to_f
+puts "You have entered #{user_time}"
+
+##############################
+
+# Challenge 2
+
+# 1. Make an array containing the following Instagram usernames...
+
+# * @womenintech
+# * @acsofficial
+# * @womenwhocode
+# * @codelikeagirlau
+
+# 2. Print the first item.
+
+# 3. Print the last item.
+
+# 4. Print the second item.
+
+# 5. Remove the last item and print it.
+
+# 6. Remove the first item and print it.
+
+# 7. Add @vict4w to the end of the array.
+
+# 8. Add @nasa to the beginning of the array.
+
+# 9. Insert @astro_jessica into the second-to-last position.
+
+# 10. Print the length of the array.
+
+
+# Sarah Connor Mode! (optional) (https://en.wikipedia.org/wiki/Sarah_Connor_(Terminator))
+
+# 1. Use a loop to do the following...
+# * get the username
+# * remove the @ character
+# * append the username to "https://instagram.com/" to create a URL
+# * print the URL
+
+# 2. Create a new array called urls. Add each of the urls to the new array as they are created.
+
+usernames.each do |username|
+
+end
+
+# 3. Print each url. Example output...
+
+# url: https://instagram.com/womenintech
+
+
+###############
+
+
+# fin
+
+# rachael.colley@coderacademy.edu.au
