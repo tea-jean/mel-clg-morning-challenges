b4112cba3c3c15ac300448a9e75fef3850444a75
burntsugar <rrakky89@gmail.com>
2019-11-01 07:51:50 +1100

    day 8 morn chall

---
+++ term-1/week-3/day-08-chall.rb
@@ -0,0 +1,94 @@
+# Morning challenges!
+
+# Challenge 1
+
+# This array stores the follower count for @michelleobama, @mariekondo and @marthastewart respectively...
+num_of_followers = [33800000, 3300000, 2600000]
+
+# We also know that @hillaryclinton has followers...
+hilary_c = 4500000
+
+# 1. Loop through each number in the num_of_followers array. The loop should print "Hilary has fewer followers". The loop should exit as soon as the comparison of her follower count to each follower count in the loop is lower.
+
+# Hint: https://www.rubyguides.com/2019/09/ruby-next-break-keywords/
+
+num_of_followers.each do |num|
+    
+end
+
+# 2. We also know that Beyonce has 134000000 followers. Write another loop which iterates through each number in the num_of_followers array. The loop should print "Beyonce has more followers". The loop should exit as soon as the comparison of her follower count to each follower count in the loop is higher.
+beyonce = 134000000
+
+# 3. Place each loop in a well-named method.
+
+# 4. Call the methods.
+
+
+######################################
+
+
+# Challenge 2
+
+# Consider the following array...
+follower_count = [33800000, 3300000, 2600000, 4500000, 134000000]
+
+# 1. Use a loop to calculate the total of all followers combined. Store the result in a well named variable.
+
+# 2. Place your code in a well-named method which returns the calculated total.
+
+# 3. Call the method.
+
+# 4. Print the result.
+
+# 5. Now go and read about Array's .sum method https://ruby-doc.org/core-2.5.1/Array.html#method-i-sum
+
+# 6. Write another implementation of this challenge using .sum.
+
+
+######################################
+
+
+# Challenge 3
+
+# The guessing loop 1.
+# This program initialises a random number between 1 - 10, and then initialises the user's guess to -1.
+
+# 1. Write a loop which asks the user to guess the number. The loop should continue 'while' their guess is incorrect.
+
+random_number = rand(11)
+# puts random_number # Uncomment this if you wish to peak at the random number!
+guess = -1
+
+
+#####################
+
+
+# Challenge 4
+
+# The guessing loop 2.
+# This program initialises a random number between 1 -10, and then initialises the user's guess to -1.
+
+# 1. Write a loop which asks the user to guess the number. The loop should 'until' their guess is correct.
+
+random_number = rand(11)
+# puts random_number # Uncomment this if you wish to peak at the random number!
+guess = -1
+
+
+####################
+
+
+# Challenge 5
+
+# 1. Write a hash which contains data about today's weather forecast for Melbourne. Use this link http://www.bom.gov.au/vic/forecasts/melbourne.shtml
+
+# 2. Iterate the hash using each to print the keys and values.
+
+
+##############
+
+
+# fin
+
+# rachael.colley@coderacademy.edu.au
+
