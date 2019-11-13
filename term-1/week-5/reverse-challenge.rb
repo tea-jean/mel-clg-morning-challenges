9bf08b18b46bae7474a8a866e29239b4f505d5ff
burntsugar <rrakky89@gmail.com>
2019-11-14 07:37:59 +1100

    added reverse challenge.md

---
+++ term-1/week-5/reverse-challenge.rb
@@ -0,0 +1,5 @@
+# You may have seen the [Ruby string reverse method](https://ruby-doc.org/core-2.5.1/String.html#method-i-reverse).
+
+# Write your own implementation of this.
+
+# Place your code in a method which takes a string argument and returns the reversed string.
